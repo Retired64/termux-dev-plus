75b8c0241a42caa464eacf502b92b8d691b059e6
TermuxDev <termuxdev00@gmail.com>
2023-12-12 15:55:37 -0500

    add server function

--- .dev/functions/create/react.sh
+++ .dev/functions/create/react.sh
@@ -4,7 +4,7 @@ source ~/.termux-dev/env/env.sh
 source ~/.termux-dev/env/config.sh
 
 echo -e -n "${green}
-ENTER NAME PROJECT: "
+ENTER NAME PROJECT: "${white}
 read -r name
 
 npx create-react-app ${name}
--- .dev/functions/server/python.sh
+++ .dev/functions/server/python.sh
@@ -2,3 +2,5 @@
 
 source ~/.termux-dev/env/env.sh
 source ~/.termux-dev/env/config.sh
+
+python3 -m http.server 8080
