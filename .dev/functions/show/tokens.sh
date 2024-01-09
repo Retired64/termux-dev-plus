2ff2c7a4a3d53060e7a324120e6be8ed6b51218b
TermuxDev <termuxdev.00@gmail.com>
2024-01-08 19:00:55 -0500

    fixed some bugs

--- .dev/functions/show/tokens.sh
+++ .dev/functions/show/tokens.sh
@@ -3,24 +3,26 @@
 source ~/.termux-dev/env/env.sh
 source ~/.termux-dev/env/config.sh
 
-if [[ ! -d ~/.termux-dev/tokens/list ]]; then
-  mkdir -p ~/.termux-dev/tokens/list
+if [[ ! -d ~/.termux-dev/tokens/.list ]]; then
+  mkdir -p ~/.termux-dev/tokens/.list
 fi
 
 names=$(ls ~/.termux-dev/tokens)
-echo "${names}" >> ~/.termux-dev/tokens/list/names.txt
-num=$(cat ~/.termux-dev/tokens/list/names.txt | wc -l)
+echo "${names}" >> ~/.termux-dev/tokens/.list/names.txt
+num=$(cat ~/.termux-dev/tokens/.list/names.txt | wc -l)
 
 for ((i = 1; i <= ${num}; i++)); do
-  name=$(awk "NR ==${i}" ~/.termux-dev/tokens/list/names.txt)
-  echo -e "${i} ${name}"
+  name=$(awk "NR ==${i}" ~/.termux-dev/tokens/.list/names.txt)
+  echo -e "${black}
+${bwhite} ${i} ${name} "${bblack}
 done
 
 echo -e -n "${white}
 ${bred} ENTER NUMBER TOKEN ${bblack}${red} "${black}
 read -r numberToken
 
-nameToken=$(awk "NR ==${numberToken}" ~/.termux-dev/tokens/list/names.txt)
+nameToken=$(awk "NR ==${numberToken}" ~/.termux-dev/tokens/.list/names.txt)
 
 echo -e ""
 cat ~/.termux-dev/tokens/${nameToken}
+rm ~/.termux-dev/tokens/.list/names.txt
