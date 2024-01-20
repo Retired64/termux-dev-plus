0501caae60677eb24ae982941de6cfbcea26a276
TermuxDev <termuxdev.00@gmail.com>
2024-01-20 15:13:24 -0500

    Fixed some bugs

--- .dev/functions/config/theme.sh
+++ .dev/functions/config/theme.sh
@@ -5,25 +5,26 @@ source ~/.termux-dev/env/config.sh
 
 echo -e -n "${red}
 
- 1 RED 
-
- 2 BLUE 
-
- 3 GREEN 
-
- 4 CYAN 
-
-NUMBER  "${white}
+${bred} ${black}1${red} ${black}RED ${bblack}${red}
+${bred} ${black}2${red} ${black}BLUE ${bblack}${red}
+${bred} ${black}3${red} ${black}GREEN ${bblack}${red}
+${bred} ${black}4${red} ${black}CYAN ${bblack}${red}
+${white}
+${bred} NUMBER ${bblack}${red} "${black}
 read -r option
 
 if [[ "${option}" == "1" ]]; then
   cp ${TermuxDev}/.dev/env/theme/red.sh ~/.termux-dev/env/config.sh
+  echo "cursor=#FF0000" > ~/.termux/colors.properties
 elif [[ "${option}" == "2" ]]; then
   cp ${TermuxDev}/.dev/env/theme/blue.sh ~/.termux-dev/env/config.sh
+  echo "cursor=#0000FF" > ~/.termux/colors.properties
 elif [[ "${option}" == "3" ]]; then
   cp ${TermuxDev}/.dev/env/theme/green.sh ~/.termux-dev/env/config.sh
+  echo "cursor=#00FF00" > ~/.termux/colors.properties
 elif [[ "${option}" == "4" ]]; then
   cp ${TermuxDev}/.dev/env/theme/cyan.sh ~/.termux-dev/env/config.sh
+  echo "cursor=#00FFFF" > ~/.termux/colors.properties
 else
   echo -e "${red}Ivalid Number!"${white}
 fi
