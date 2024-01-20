0c960fe1fdaafa02979109692aac5757a426c5dd
TermuxDev <termuxdev.00@gmail.com>
2024-01-20 00:21:41 -0500

    Add function: create project

---
+++ .dev/functions/new/project.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ~/.termux-dev/env/env.sh
+source ~/.termux-dev/env/config.sh
+
+echo -e -n "${red}
+${bred} ${white}CHOOSE YOUR FRAMEWORK ${bblack}${red}
+${white}
+${bwhite} ${red}1${white} ${red}VITE ${bblack}${white}
+${bwhite} ${red}2${white} ${red}CREATE-REACT-APP ${bblack}${white}
+
+${white}
+${bred} NUMBER ${bblack}${red} "${black}
+read -r framework
+
+if [[ "${framework}" == "1" ]]; then
+  npm create vite
+elif [[ "${framework}" == "2" ]]; then
+  echo -e -n "${red}Enter name project: "${white}
+  read -r nameProject
+  npx create-react-app ${nameProject}
+else
+  echo -e "${red}Invalid number!"${white}
+  exit
+fi
