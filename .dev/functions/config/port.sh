2cca3a071e139f016b2779027dff2758f4d04df1
TermuxDev <termuxdev.00@gmail.com>
2024-01-20 23:19:31 -0500

    Fixed some bugs

--- .dev/functions/config/port.sh
+++ .dev/functions/config/port.sh
@@ -7,10 +7,15 @@ if [[ ! -d ~/.termux-dev/env/ports ]]; then
   mkdir -p ~/.termux-dev/env/ports
 fi
 
+if [[ -f ~/.termux-dev/env/ports/port.txt ]]; then
+  oldPort=$(cat ~/.termux-dev/env/ports/port.txt)
+else
+  oldPort="8080"
+fi
 echo -e -n "${white}
 ${bred} PORT NUMBER ${bblack}${red} "${black}
 read -r port
 
 echo "${port}" > ~/.termux-dev/env/ports/port.txt
-sed -i "s!8080!${port}!g" ~/.termux-dev/env/config.sh
+sed -i "s!${oldPort}!${port}!g" ~/.termux-dev/env/config.sh
 
