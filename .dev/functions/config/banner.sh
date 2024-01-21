95f59702f0bcc85a525abf88eba43abbceeafd47
TermuxDev <termuxdev.00@gmail.com>
2024-01-21 18:58:00 -0500

    Add new function: change banner

--- .dev/functions/config/banner.sh
+++ .dev/functions/config/banner.sh
@@ -2,3 +2,10 @@
 
 source ~/.termux-dev/env/env.sh
 source ~/.termux-dev/env/config.sh
+
+echo -e -n "${white}
+${bred} ENTER PATH FILE (.txt) ${red}${bblack} "${black}
+read -r filePath
+
+cp ${filePath} ~/.termux-dev/custom/custom.txt
+echo "source ~/.termux-dev/custom/custom.txt" > ~/.termux-dev/custom/banner.sh
