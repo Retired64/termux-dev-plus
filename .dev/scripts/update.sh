12609082868dce463ded604eff814ed9300703bb
TermuxDev <termuxdev.00@gmail.com>
2024-01-21 19:54:08 -0500

    Fixed some bugs

--- .dev/scripts/update.sh
+++ .dev/scripts/update.sh
@@ -18,14 +18,13 @@ if [[ -f ~/.termux-dev/custom/custom.txt ]]; then
   cp -r ${TermuxDev}/.dev/custom ~/.termux-dev
   cp -r ${TermuxDev}/.dev/custom/.termux ~
   mv ~/.termux-dev/custom/backup.sh ~/.termux-dev/custom/banner.sh
-fi
-
+else
 # ACTUALIZANDO BANNER TERMUX-DEV 
-
-cp ${TermuxDev}/.dev/bin/* $PREFIX/bin
-cp ${TermuxDev}/.dev/env/config.sh ~/.termux-dev/env/config.sh
-cp -r ${TermuxDev}/.dev/custom ~/.termux-dev
-cp -r ${TermuxDev}/.dev/custom/.termux ~
+  cp ${TermuxDev}/.dev/bin/* $PREFIX/bin
+  cp ${TermuxDev}/.dev/env/config.sh ~/.termux-dev/env/config.sh
+  cp -r ${TermuxDev}/.dev/custom ~/.termux-dev
+  cp -r ${TermuxDev}/.dev/custom/.termux ~
+fi
 
 # VERIFICANDO TEMA SELECCIONADO 
 
