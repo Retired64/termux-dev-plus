fbe47b7d2cdd26b324da55c040690603387b9799
TermuxDev <termuxdev.00@gmail.com>
2024-02-20 15:12:23 -0500

    Improving the jsx code formatter

--- .dev/custom/banner.sh
+++ .dev/custom/banner.sh
@@ -7,7 +7,7 @@ clear
 cat ~/.termux-dev/custom/termux-dev.txt
 echo -e "         ${white}${bwhite}${black}    WELCOME TO TERMUX-DEV!   ${bblack}${white}
 
-              ${red}${bred}${white}  TERMUX-DEV 1.3.3  ${bblack}${red}
+              ${red}${bred}${white}  TERMUX-DEV 1.4.3  ${bblack}${red}
 
                     ${white}${bwhite}${black}  </>  ${bblack}${white}
 "
