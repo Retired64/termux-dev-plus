dd80dd5ec5106bff23913fd019967909f7aab6d6
TermuxDev <termuxdev.00@gmail.com>
2024-04-12 00:26:59 -0500

    Add New Version (NvChad)

--- install.sh
+++ install.sh
@@ -81,7 +81,7 @@ fi
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
 
-git clone https://github.com/NvChad/starter ~/.config/nvim
+git clone https://github.com/TermuxDev/termux-dev-repo.git ~/configNvChad && cp -r ~/configNvChad/install/nvchad/nvim ~/.config/nvim && rm -rf ~/configNvChad
 
 # INSTALANDO OH-MY-ZSH
 
